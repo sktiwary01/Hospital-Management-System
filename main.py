0200047e883105dbfda6e9d85c2ceb9a6d02dc84
sktiwary01 <shiwamsudhanshu1234@gmail.com>
2026-05-01 16:09:15 +0530

    final deploy fix

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'shiwamsudhanshu1234@gmail.com'
-app.config['MAIL_PASSWORD'] = 'abcdefghijk'
+app.config['MAIL_PASSWORD'] = 'fnfcnvitwtydfqdk'
 mail = Mail(app)
 
 
